44e27e4276f1ab9d9316dc0bd6049f8b66d31c4f
denismaster <denismaster@outlook.com>
2019-06-26 18:34:46 +0300

    Sync order state using firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -9,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuCategoryComponent } from './menu/menu-category/menu-category.component';
 import { MenuCategoryItemComponent } from './menu/menu-category-item/menu-category-item.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import { MenuCategoryItemComponent } from './menu/menu-category-item/menu-catego
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-    ])
+    ]),
+    AngularFireModule.initializeApp(environment.firebase, 'praga-menu'),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { v4 as uuid } from 'uuid';
-import { MENU } from '../products';
+import { MENU, OrderUpdate } from '../products';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { filter } from 'rxjs/operators'
+import * as firebase from 'firebase';
+
+export interface OrderItem { id:string, name: string; category: string, price: number }
+export interface Order { dishes: OrderItem[] }
 
 @Component({
   selector: 'app-home',
@@ -11,16 +18,91 @@ export class HomeComponent {
   menu = MENU;
   orderId: string;
 
-  constructor(activatedRoute: ActivatedRoute, router: Router) {
+  order$: Observable<Order>;
+
+  dishes: OrderItem[] = [];
+
+  constructor(activatedRoute: ActivatedRoute, router: Router, private afs: AngularFirestore) {
     const order = activatedRoute.snapshot.queryParams.order;
 
     if (!order) {
-      const generatedId = uuid();
-      router.navigate(['/'], { queryParams: { 'order': generatedId } });
+      const generatedId  = uuid();
+      this.orderId = generatedId;
+      this.afs.collection("orders").doc<Order>(generatedId).set({ dishes: [] }).then(_=>{
+        router.navigate(['/'], { queryParams: { 'order': generatedId } });
+      })
     }
 
     this.orderId = order;
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const orderDoc = this.afs.doc<Order>(`orders/${this.orderId}`);
+    this.order$ = orderDoc.valueChanges();
+    this.order$.pipe(filter(order => !!order)).subscribe(order => {
+      this.dishes = order.dishes;
+    })
+  }
+
+  get categorizedDishes() {
+    return this.dishes.reduce((prev, current) => {
+      if (!prev[current.category]) {
+        prev[current.category] = [{
+          name: current.name,
+          amount: 1
+        }]
+      }
+      else {
+        let productsInCategory: { name: string, amount: number }[] = prev[current.category];
+        let currentDishIndex = productsInCategory.findIndex(p => p.name == current.name)
+        if (currentDishIndex !== -1) {
+          productsInCategory[currentDishIndex].amount++;
+        }
+        else {
+          productsInCategory.push({
+            name: current.name,
+            amount: 1
+          });
+        }
+
+      }
+      return prev;
+    }, {});
+  }
+
+  get dishCategories() {
+    return Object.keys(this.categorizedDishes);
+  }
+
+  get totalPrice() {
+    return this.dishes.reduce((p, c) => {
+      return p + c.price
+    }, 0)
+  }
+
+  updateOrder(update: OrderUpdate) {
+    if (update.action == "add") {
+      this.afs.doc<Order>(`orders/${this.orderId}`).update({
+        dishes: <any>firebase.firestore.FieldValue.arrayUnion({
+          id: uuid(),
+          name: update.name,
+          category: update.category,
+          price: update.price
+        })
+      })
+    }
+    else {
+      const dishIds = this.dishes.filter(d=>d.name==update.name).map(d=>d.id);
+      const idToRemove = dishIds[0];
+      if(!idToRemove) return;
+      this.afs.doc<Order>(`orders/${this.orderId}`).update({
+        dishes: <any>firebase.firestore.FieldValue.arrayRemove({
+          id: idToRemove,
+          name: update.name,
+          category: update.category,
+          price: update.price
+        })
+      })
+    }
+  }
 }
